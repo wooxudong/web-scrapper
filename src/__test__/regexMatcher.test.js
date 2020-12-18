3f63f90d919248cc2cc2933b3723e6c8eba02f8a
Wu Xudong <wooxudong@gmail.com>
2020-12-18 16:26:25 +0800

    added scrapper for the incentives

--- src/__test__/regexMatcher.test.js
+++ src/__test__/regexMatcher.test.js
@@ -20,6 +20,11 @@ describe('the regex matcher', ()=>{
 
   it('should return correct number if there is no % sign attched in the end', ()=>{
     const input = "88.90% adganc";
-    expect(aprMatcher.match(input)).toEqual('88.90%');
+    expect(aprMatcher.match(input)).toEqual(['88.90%']);
+  });
+
+  it('should return correct number if there is no % sign attched in the end', ()=>{
+    const input = "APY:0.70%\\n+20.43% MTA\\n+2.01%\\nto 5.02%  CRV";
+    expect(aprMatcher.match(input)).toEqual(['0.70%', '20.43%', '2.01%', '5.02%']);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 const puppeteer = require('puppeteer');
+const aprMatcher = require('./aprMatcher');
 
-
-(async () => {
-  const aprMatcher = require('./aprMatcher');
+const getRawTexts = async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.curve.fi/');
-  const closeButton = await page.$('div.bn-onboard-modal-content-close')
-  await closeButton.evaluate(closeButton => closeButton.click());
+  try {
+    const aprMatcher = require('./aprMatcher');
+    const page = await browser.newPage();
+    await page.goto('https://www.curve.fi/');
+    const closeButton = await page.$('div.bn-onboard-modal-content-close')
+    await closeButton.evaluate(closeButton => closeButton.click());
 
-  const texts = await page.evaluate(() => Array.from(document.querySelectorAll('span.apr'), element => {
-    const rawTexts = [];
-    for (const child of element.children) {
-      rawTexts.push(child.innerText);
-    }
-    return rawTexts;
-  }));
+    //it took a little bit of time for curve.fi to load the incentives.
+    await page.waitForTimeout(5000);
+    const texts = await page.evaluate(() => Array.from(document.querySelectorAll('span.apr'), element => {
+      return element.innerText;
+    }));
+    await browser.close();
+    return texts;
+  } catch (e) {
+    return [];
+  } finally {
+    await browser.close();
+  }
 
-  const aprs = texts.flatMap(arr => arr.join()).map(apr => aprMatcher.match(apr)).filter(it => it);
+};
+(async function (){
+  const rawTexts = await getRawTexts();
+  const aprs = rawTexts.map(apr => aprMatcher.match(apr)).filter(it => it);
   console.log(aprs);
-  await browser.close();
 })();
 
 
