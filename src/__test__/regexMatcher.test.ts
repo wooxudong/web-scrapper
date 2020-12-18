b3396a4226712a967ae83b1c920ee9dfc8af277c
Wu Xudong <wooxudong@gmail.com>
2020-12-18 18:32:41 +0800

    coverting to typescript

--- src/__test__/regexMatcher.test.ts
+++ src/__test__/regexMatcher.test.ts
@@ -1,30 +1,24 @@
+import {match} from '../aprMatcher';
 
-const aprMatcher = require('../aprMatcher');
 describe('the regex matcher', ()=>{
-
-  it('should return null if input is null', ()=>{
-    const input = null;
-    expect(aprMatcher.match(input)).toBeNull();
-  });
-
 
   it('should return null if the there is no numbers', ()=>{
     const input = 'abc';
-    expect(aprMatcher.match(input)).toBeNull();
+    expect(match(input)).toBeNull();
   });
 
   it('should return null if there is no % sign attched in the end', ()=>{
     const input = "88.90";
-    expect(aprMatcher.match(input)).toBeNull();
+    expect(match(input)).toBeNull();
   });
 
   it('should return correct number if there is no % sign attched in the end', ()=>{
     const input = "88.90% adganc";
-    expect(aprMatcher.match(input)).toEqual(['88.90%']);
+    expect(match(input)).toEqual(['88.90%']);
   });
 
   it('should return correct number if there is no % sign attched in the end', ()=>{
     const input = "APY:0.70%\\n+20.43% MTA\\n+2.01%\\nto 5.02%  CRV";
-    expect(aprMatcher.match(input)).toEqual(['0.70%', '20.43%', '2.01%', '5.02%']);
+    expect(match(input)).toEqual(['0.70%', '20.43%', '2.01%', '5.02%']);
   });
 });
--- src/aprMatcher.js
+++
@@ -1,9 +0,0 @@
-
-function match(raw) {
-  const regexForTarget = /[0-9]*\.[0-9]*%/gi;
-  if (!raw) return null;
-  const matched = [...raw.matchAll(regexForTarget)];
-  return matched && matched.length > 0 ? matched.map(item=>item.pop()) : null;
-}
-
-exports.match = match;
